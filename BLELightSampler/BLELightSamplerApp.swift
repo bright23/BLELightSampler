08b6fbf271ead1830c68ccbdcba3e1975598a1c9
Wataru Fujiwara <wataru930@gmail.com>
2025-08-13 18:44:47 +0900

    Initial Commit

---
+++ BLELightSampler/BLELightSamplerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLELightSamplerApp.swift
+//  BLELightSampler
+//
+//  Created by Wataru Fujiwara on 2025/08/13.
+//
+
+import SwiftUI
+
+@main
+struct BLELightSamplerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
